abca3c740e0d9e07cd21fc1e4690c6589cf8fb21
Kemal Delalic <kemal.delalic@gmail.com>
2012-01-07 15:21:58 +0100

    Replaced Arr::values() call with array_values() + Arr::extract() equivalent, fixed tables layout issue

--- classes/controller/admin/media.php
+++ classes/controller/admin/media.php
@@ -2,19 +2,25 @@
 
 class Controller_Admin_Media extends Controller_Admin {
 
+	/**
+	 * Action for serving media files with HTTP caching enabled
+	 *
+	 * @throws	HTTP_Exception_404
+	 */
 	public function action_get()
 	{
 		// Get the filename & extension from requested filenames pathinfo
 		$info = pathinfo($this->request->param('file'));
 		
-		list($filename, $ext, $directory) = Arr::values($info,
-			array('filename','extension','dirname'));
+		list($filename, $ext, $directory) = array_values(Arr::extract($info,
+			array('filename','extension','dirname')));
 			
 		$directory = $directory ? "media/{$directory}" : 'media';
 		
-		if ($file = Kohana::find_file($directory, $filename, $ext ?: EXT))
+		if ($file = Kohana::find_file($directory, $filename, $ext))
 		{
-			return $this->response->body(file_get_contents($file))
+			return $this->response
+				->body(file_get_contents($file))
 				->headers('cache-control','public, max-age='.Date::WEEK)
 				->headers('content-type', File::mime($file))
 				->check_cache(NULL, $this->request);
